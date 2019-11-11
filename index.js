b74cd488af7dc9ea8b2874008f4e1ceae6483346
Armel <armelfli50@gmail.com>
2019-11-11 11:25:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+countDown = function(arg){
+    for(let i = 0; i<= arg; i= i+1){
+        console.log(i)
+    }
+}
+function writeCards(arg){
+    const result = [];
+    for(let i = 0; i < arg.length; i++){
+       result.push(`Thank you, ${arg[i]}, for the wonderful surprise gift!`)
+    }
+    return result
+}
